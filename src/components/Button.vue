8a9c016337bc9794a24dc164c10187b9df84d6e5
Eric Henderson <ehenderson828@gmail.com>
2023-12-28 20:30:20 -0500

    test

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <!-- 'v-on:' and '@' trigger events in the same way -->
+    <button v-on:click="onClick()" class="btn" :style="{ background: color }">{{ text }}</button>
+</template>
+
+<script>
+
+    export default {
+        name: 'Button',
+        props: {
+            text: String,
+            color: String
+        },
+        methods: {
+            onClick() {
+                console.log('click');
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
